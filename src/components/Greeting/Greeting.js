8a947495d2091da2c80f763d460328278c965686
satish d <satsbelfast@gmail.com>
2022-11-21 12:38:19 +0000

    usestate

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,10 +1,22 @@
 // function Greeting(){
 //     return <p>Hello</p>;
 // }
+import { useState } from 'react';
 import './Greeting.css'
 
 const Greeting = (props) => {
-    return <p className="greeting-paragraph">Hello {props.name}, you are {props.age} years old</p>;
+
+    //let name = props.name;
+    const [name, setName] = useState(props.name);
+
+    const changeName= () => {
+        console.log("button was clicked");
+        setName("Sally");
+    }
+    return <div>
+                <p className="greeting-paragraph">Hello {name}, you are {props.age} years old</p>
+                <button onClick={changeName}>Change the name</button>
+            </div>;
 }
 
 export default Greeting;
--- src/components/Songs/SongList.js
+++ src/components/Songs/SongList.js
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import Song from "./Song";
+import './Songs.css';
 const SongList = () => {
+    
+        const [visible, setVisible] = useState(false);
+        //let buttonText = "Show songs";
+        //let ulClass = "songs-hidden";
+
+        //if (visible === true){
+        //    buttonText = "Hide songs";
+        //    ulClass = "songs-display";
+        //}
+        //
+        const toggleVisibility= () => {
+            setVisible(!visible);
+        }
+
+
     return (
         <div>
             <h1>Your favourite songs are:</h1>
-            <u1>
+            <button onClick={toggleVisibility} >{visible ? "Hide" : "Show"} songs</button>
+            { 
+            //<ul className = {ulClass} > 
+            }
+            <ul style={{display:visible ? "block" : "none"}}>
                 <Song title="Last thing on my mind" artist="Steps" />
                 <Song title ="If you're over me" artist="Years and Years" />
-            </u1>
-
-
+            </ul>
         </div>
     );
 }
